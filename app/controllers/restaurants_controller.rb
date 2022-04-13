c29135ea3b717774ada3c04f8c9e89a77cebcff6
Wendy Pham <wendypham_s2@hotmail.com>
2022-04-12 21:15:52 -0400

    created strong params, and new page/form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,9 +7,26 @@ class RestaurantsController < ApplicationController
 
   def show; end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.valid?
+      @restaurant.save
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
   end
 end
